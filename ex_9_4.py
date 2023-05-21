12c233c0b0eac68b21146004e49133e1515c5843
Hassan EL BAHI <me_80_me@hotmail.com>
2023-05-21 23:23:18 +0100

    Finished ex 9.4

--- ex_9_4.py
+++ ex_9_4.py
@@ -1,16 +1,24 @@
 name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
-handle = open(name)
-counts = dict()
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+# Create a dictionary to store the word counts
+count = {}
+
+# Open the file and loop over each line
+with open(name, 'r') as file:
+    for line in file:
+        # Check if the line starts with "From" and has at least two words
+        if line.startswith('From ') and len(line.strip().split()) >= 2:
+            # Split the line into words
+            words = line.strip().split()
+
+            # Get the second word and add it to the word count dictionary
+            second_word = words[1]
+            count[second_word] = count.get(second_word, 0) + 1
 
 bigcount = None
 bigword = None
-for word, count in counts.items():
+for word, count in count.items():
     if bigcount is None or count > bigcount:
         bigcount = count
         bigword = word
